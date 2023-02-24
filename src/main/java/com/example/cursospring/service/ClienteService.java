6852156b683182556b10c6b74ce85575ab5092c5
thony239 <anthony.morocho.est@tecazuay.edu.ec>
2023-02-23 20:56:04 -0500

    a

--- src/main/java/com/example/cursospring/service/ClienteService.java
+++ src/main/java/com/example/cursospring/service/ClienteService.java
@@ -1,4 +1,16 @@
 package com.example.cursospring.service;
 
+import com.example.cursospring.entity.Cliente;
+import com.example.cursospring.entity.Proveedor;
+
+import java.util.List;
+
 public interface ClienteService {
+    public List<Cliente> findAll();
+
+    public Cliente  save(Cliente c);
+
+    public Cliente findById(Integer id);
+
+    public void delete(Integer id);
 }
--- src/main/java/com/example/cursospring/service/ClienteServiceImp.java
+++ src/main/java/com/example/cursospring/service/ClienteServiceImp.java
@@ -1,4 +1,35 @@
 package com.example.cursospring.service;
 
-public class ClienteServiceImp {
+import com.example.cursospring.entity.Cliente;
+import com.example.cursospring.entity.Proveedor;
+import com.example.cursospring.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ClienteServiceImp implements ClienteService{
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> findAll() {
+        return (List<Cliente>) clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente save(Cliente c) {
+        return clienteRepository.save(c);
+    }
+
+    @Override
+    public Cliente findById(Integer id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        clienteRepository.deleteById(id);
+    }
 }
